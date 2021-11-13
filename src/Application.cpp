8abb78021e54a1422c63b7cd11d1327a233cd685
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2021-11-13 10:50:47 +0100

    Add glm and play around with some math

--- src/Application.cpp
+++ src/Application.cpp
@@ -10,6 +10,9 @@
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
 
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+
 int main(void) {
   GLFWwindow *window;
 
@@ -64,9 +67,12 @@ int main(void) {
 
   IndexBuffer ib(indices, 6);
 
+  glm::mat4 proj = glm::ortho(-1.0f, 1.0f, -1.325f, 1.325f, -1.0f, 1.0f);
+
   Shader shader("res/shaders/Basic.shader");
   shader.Bind();
   shader.SetUniform4f("u_Color", 0.8f, 0.2f, 0.3f, 1.0f);
+  shader.SetUniformMat4f("u_MVP", proj);
 
   Texture texture("res/textures/b738.png");
   texture.Bind();
--- src/Shader.h
+++ src/Shader.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <glm.hpp>
 #include <string>
 #include <unordered_map>
 
@@ -25,6 +26,7 @@ public:
   void SetUniform1f(const std::string &name, float value);
   void SetUniform4f(const std::string &name, float v0, float v1, float v2,
                     float v3);
+  void SetUniformMat4f(const std::string &name, const glm::mat4 &matrix);
 
 private:
   int GetUniformLocation(const std::string &name);
