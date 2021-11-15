1a8aff7a996cf2cb20a5d306633482e90e9d0e13
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2021-11-15 22:41:13 +0100

    Improve Triangle test, don't do everything every frame.

--- src/test/TestTriangle.cpp
+++ src/test/TestTriangle.cpp
@@ -1,15 +1,22 @@
 #include "TestTriangle.h"
 
-#include "Renderer.h"
-#include "VertexBufferLayout.h"
 #include "imgui/imgui.h"
 #include <iostream>
 
 namespace test {
 
 TestTriangle::TestTriangle()
-    : m_Positions{-1.0f, -1.0f, 1.0f, -1.0f, -0.5f, 1.0f}, m_Indices{0, 1, 2},
-      m_Color{1.0f, 1.0f, 1.0f, 1.0f} {}
+    : m_Positions{-1.0f, -1.0f, 1.0f, -1.0f, -0.5f, 1.0f}, m_Color{1.0f, 1.0f,
+                                                                   1.0f, 1.0f} {
+  unsigned int indices[] = {0, 1, 2};
+
+  m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 3);
+
+  m_VertexBufferLayout = std::make_unique<VertexBufferLayout>();
+  m_VertexBufferLayout->Push<float>(2);
+
+  m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
+}
 
 TestTriangle::~TestTriangle() {}
 
@@ -17,19 +24,15 @@ void TestTriangle::OnUpdate(float deltaTime) {}
 
 void TestTriangle::OnRender() {
   VertexBuffer vb(m_Positions, 3 * 2 * sizeof(float));
-  VertexBufferLayout layout;
-  layout.Push<float>(2);
-  IndexBuffer ib(m_Indices, 3);
   VertexArray va;
-  va.AddBuffer(vb, layout);
+  va.AddBuffer(vb, *m_VertexBufferLayout);
 
-  Shader shader("res/shaders/Basic.shader");
-  shader.Bind();
-  shader.SetUniform4f("u_Color", m_Color[0], m_Color[1], m_Color[2],
-                      m_Color[3]);
+  m_Shader->Bind();
+  m_Shader->SetUniform4f("u_Color", m_Color[0], m_Color[1], m_Color[2],
+                         m_Color[3]);
 
   Renderer renderer;
-  renderer.Draw(va, ib, shader);
+  renderer.Draw(va, *m_IndexBuffer, *m_Shader);
 }
 
 void TestTriangle::OnImGuiRender() {
--- src/test/TestTriangle.h
+++ src/test/TestTriangle.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "Renderer.h"
 #include "Test.h"
+#include "VertexBufferLayout.h"
 
 namespace test {
 
@@ -15,8 +17,11 @@ public:
 
 private:
   float m_Positions[6];
-  unsigned int m_Indices[3];
   float m_Color[4];
+
+  std::unique_ptr<IndexBuffer> m_IndexBuffer;
+  std::unique_ptr<VertexBufferLayout> m_VertexBufferLayout;
+  std::unique_ptr<Shader> m_Shader;
 };
 
 } // namespace test
