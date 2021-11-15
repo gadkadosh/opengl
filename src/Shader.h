8049dd9c40ae137ec03b28b5bc798952b5b81363
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2021-11-15 21:41:35 +0100

    Set GetUniformLocation to be const and m_UniformLocationCache mutable

--- src/Shader.h
+++ src/Shader.h
@@ -13,7 +13,7 @@ class Shader {
 private:
   std::string m_FilePath;
   unsigned int m_RendererID;
-  std::unordered_map<std::string, int> m_UniformLocationCache;
+  mutable std::unordered_map<std::string, int> m_UniformLocationCache;
 
 public:
   Shader(const std::string &filepath);
@@ -29,7 +29,7 @@ public:
   void SetUniformMat4f(const std::string &name, const glm::mat4 &matrix);
 
 private:
-  int GetUniformLocation(const std::string &name);
+  int GetUniformLocation(const std::string &name) const;
   unsigned int CreateShader(const std::string &vertexShader,
                             const std::string &fragmentShader);
   unsigned int CompileShader(unsigned int type, const std::string &source);
